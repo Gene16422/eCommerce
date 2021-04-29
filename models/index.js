108f538c82bfbe9c15e54c2b138586284b8316c5
Gene <egarnes20010@gmail.com>
2021-04-29 09:43:31 -0700

    initial commit

---
+++ models/index.js
@@ -0,0 +1,27 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongs to Category
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+});
+// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+});
+// Products belong to many Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+});
+// Tags belong to many Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+});
+
+module.exports = {Product, Category, Tag, ProductTag,
+
+};
